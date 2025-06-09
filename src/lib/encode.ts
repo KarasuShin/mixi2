c3e5609d485b56b138ab0d168c7c326ac63a77f2
karasu <karasu.shing@gmail.com>
2025-06-09 17:25:31 +0800

    refactor: remove convertToSnakeCase

--- src/lib/encode.ts
+++ src/lib/encode.ts
@@ -4,36 +4,13 @@ import { proto } from '~/proto'
 export function encode<T extends Record<string, any> = Record<string, any>>(data: T, type: string): Promise<Uint8Array> {
   return new Promise<Uint8Array>((resolve, reject) => {
     const root = protobuf.parse(proto).root
-    const snakeCaseData = convertToSnakeCase(data)
     const messageType = root.lookupType(`mixi.${type}`)
-    const errMsg = messageType.verify(snakeCaseData)
+    const errMsg = messageType.verify(data)
     if (errMsg) {
       reject(new Error(errMsg))
       return
     }
 
-    const message = messageType.create(snakeCaseData)
-    const encoded = messageType.encode(message).finish()
-    resolve(encoded)
+    resolve(messageType.encode(data).finish())
   })
-}
-
-function convertToSnakeCase(obj: any): any {
-  if (obj === null || typeof obj !== 'object') {
-    return obj
-  }
-
-  if (Array.isArray(obj)) {
-    return obj.map(convertToSnakeCase)
-  }
-
-  const result: any = {}
-  for (const key in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, key)) {
-      const snakeKey = key.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`)
-      result[snakeKey] = convertToSnakeCase(obj[key])
-    }
-  }
-
-  return result
 }
